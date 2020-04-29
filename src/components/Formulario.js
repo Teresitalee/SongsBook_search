2af9490e8647dc78d188dade1634f3b4d2512fbc
Teresita Madriaza <teresita@MacBook-Pro-de-Teresita.local>
2020-04-29 19:15:36 -0400

    css html

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -2,7 +2,52 @@ import React from 'react';
 
 const Formulario = () => {
     return ( 
-        <h1>Formulario</h1>
+        <div className="bg-dark">
+            <div className="container">
+                <div className="row">
+                    <form
+                      className="col card text-white bg-transparent mb-5 pt-5 pb-2"
+
+                    
+                    >
+                        <fieldset>
+                            <legend className="text-center">Busca la letra de tu canción favorita</legend>
+
+                            <div className="row">
+                                <div className="col-md-6 ">
+                                    <div className="form-group">
+                                        <label>Artist</label>
+                                        <input 
+                                            type="text"
+                                            className="form-control"
+                                            name="artista" 
+                                            placeholder="Name artist"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="col-md-6 ">
+                                    <div className="form-group">
+                                        <label>Song</label>
+                                        <input 
+                                            type="text"
+                                            className="form-control"
+                                            name="cancion" 
+                                            placeholder="Name song"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                            <button 
+                                type="submit"
+                                className="btn btn-warning float-right"
+                                >
+                                Search</button>
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
      );
 }
  
