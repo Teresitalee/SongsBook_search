43ab5cae39bc734e2bcb3394eac74f91fcbb0de3
Teresita Madriaza <teresita@MacBook-Pro-de-Teresita.local>
2020-05-02 22:26:17 -0400

    componente Info

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import Formulario from './components/Formulario';
+import Cancion from './components/Cancion';
 import axios from 'axios';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   const [ busquedaletra, guardarBusquedaLetra ] = useState({});
 
   const [ letra, guardarLetra] = useState ('');
+  const [ info, guardarInfo] = useState({});
 
   useEffect(() => {
     if(Object.keys(busquedaletra).length === 0 ) 
@@ -17,10 +19,15 @@ function App() {
       const consultarApiLetra = async () => {
         const { artista , cancion } = busquedaletra;
         const url = `https://api.lyrics.ovh/v1/${artista}/${cancion}`;
-
-        const resultado = await axios(url);
-
-        guardarLetra(resultado.data.lyrics);
+        const url2 = `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${artista}`;
+        
+        const [letra, informacion] = await Promise.all([
+          axios(url),
+          axios(url2)
+        ]);
+        
+        guardarLetra(letra.data.lyrics);
+        guardarInfo(informacion.data.artist[0]);
 
       }
       consultarApiLetra();
@@ -42,6 +49,11 @@ function App() {
             
           </div>
           <div className="col-md-6">
+            <Cancion
+              letra={letra}
+
+            
+            />
             
             
           </div>
@@ -53,3 +65,4 @@ function App() {
 }
 
 export default App;
+
