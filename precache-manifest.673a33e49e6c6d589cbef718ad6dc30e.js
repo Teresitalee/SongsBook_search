a6d6b7337af62ee7c71f6fc0001e7d7d29c6dd50
Teresita Madriaza <teresita@MacBook-Pro-de-Teresita.local>
2020-05-04 18:21:45 -0400

    Updates

---
+++ precache-manifest.673a33e49e6c6d589cbef718ad6dc30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c2107d37ad4c29b80905c8fe235ca1",
+    "url": "/SongsBook_search/index.html"
+  },
+  {
+    "revision": "a4ed81f4cfe17e1330e2",
+    "url": "/SongsBook_search/static/css/main.82b6b87a.chunk.css"
+  },
+  {
+    "revision": "82c88c9a25be6834f8b2",
+    "url": "/SongsBook_search/static/js/2.a5d732e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SongsBook_search/static/js/2.a5d732e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ed81f4cfe17e1330e2",
+    "url": "/SongsBook_search/static/js/main.cded1b20.chunk.js"
+  },
+  {
+    "revision": "932599d0727af5d03370",
+    "url": "/SongsBook_search/static/js/runtime-main.391c84bf.js"
+  }
+]);
